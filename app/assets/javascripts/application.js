440766e7884ee34f2a52f838e10df2c06a4c799b
Philipp Tessenow <philipp.tessenow@student.hpi.uni-potsdam.de>
2012-10-12 17:11:43 +0200

    use underscore.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,7 @@
 //= require 'breadcrumb'
 //= require 'chosen'
 //= require 'context_menu'
+//= require underscore
 
 //source: http://stackoverflow.com/questions/8120065/jquery-and-prototype-dont-work-together-with-array-prototype-reverse
 if (typeof []._reverse == 'undefined') {
